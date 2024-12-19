ac744feee970bd9742415be00cfcddc5d3f2e4d3
Aarayn10-sudo <lazyfox916@gmail.com>
2024-12-19 10:40:40 +0545

    Atlas Connected

--- server/db/connectMongoose.js
+++ server/db/connectMongoose.js
@@ -2,6 +2,8 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   await mongoose
-    .connect("mongodb://localhost:27017/n9-lms")
+    .connect(
+      `mongodb+srv://lazyfox916:lms-n9@lms-n9.x4ejl.mongodb.net/?retryWrites=true&w=majority&appName=lms-n9`
+    )
     .then(() => console.log("MongoDB connected..."));
 };
