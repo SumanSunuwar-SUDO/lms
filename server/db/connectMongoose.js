a8938764df64cb95619e3315cacfc329b5619fb7
Aarayn10-sudo <lazyfox916@gmail.com>
2024-12-19 10:55:09 +0545

    Removed node_modules

--- server/db/connectMongoose.js
+++ server/db/connectMongoose.js
@@ -1,9 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-  await mongoose
-    .connect(
-      `mongodb+srv://lazyfox916:lms-n9@lms-n9.x4ejl.mongodb.net/?retryWrites=true&w=majority&appName=lms-n9`
-    )
-    .then(() => console.log("MongoDB connected..."));
+  await mongoose.connect().then(() => console.log("MongoDB connected..."));
 };
