60b045e3a2f5b1503ad32d408f314f08d99cf2d0
Aarayn10-sudo <lazyfox916@gmail.com>
2024-12-19 14:59:19 +0545

    moved directory inside src

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -49,7 +49,18 @@ function App() {
           );
         })}
       </aside>
-      <main className="h-screen"></main>
+      <main className="h-screen p-[30px]">
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/tdbD2naYwdo?si=x4oIyO-wUwalTpdq"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerpolicy="strict-origin-when-cross-origin"
+          allowfullscreen
+        ></iframe>
+      </main>
     </div>
   );
 }
