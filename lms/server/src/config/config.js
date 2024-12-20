5c3f2b83b67e5fd310ef7e7666b951e2f73610c2
Aaryan Sharma <144255729+Aaryan10-sudo@users.noreply.github.com>
2024-12-20 14:58:55 +0545

    Delete lms directory

--- lms/server/src/config/config.js
+++
@@ -1,7 +0,0 @@
-import { config } from "dotenv";
-
-config();
-export const databaseURI = process.env.MONGODB_URL;
-export const secretKey = process.env.SECRET_KEY;
-export const email = process.env.EMAIL_ADDRESS;
-export const password = process.env.EMAIL_APP_PASSWORD;
--- lms/server/src/db/connectMongoose.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-import { databaseURI } from "../config/config.js";
-
-export const connectDB = async () => {
-  await mongoose
-    .connect(databaseURI)
-    .then(() => console.log("MongoDB connected..."));
-};
